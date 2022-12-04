c80d42a8b3346c805c9ec5164cab55591aecaf34
Luke Dunekacke <ldunekacke@gmail.com>
2022-12-04 12:10:12 -0700

    down to 164 chars

--- day4/day4_golf.py
+++ day4/day4_golf.py
@@ -1,3 +1,3 @@
 import re
-c=[[int(x)for x in re.split(',|-',l)]for l in open('a').readlines()]
-print(sum((x[0]>=x[2]and x[1]<=x[3])or(x[2]>=x[0]and x[3]<=x[1])for x in c),sum((x[0]<=x[2]<=x[1])or(x[2]<=x[0]<=x[3])for x in c))
+i=[[int(x)for x in re.split(',|-',l)]for l in open('a').readlines()]
+print(sum(c<=a and b<=d for a,b,c,d in i),sum(a<=c<=b or c<=a<=d for a,b,c,d in i))
