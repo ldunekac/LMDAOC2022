3cf4460404d69f2447f54a1cb6aa81c258489585
Luke Dunekacke <ldunekacke@gmail.com>
2022-12-04 11:19:40 -0700

    day 4 golf refactor

--- day4/day4_golf.py
+++ day4/day4_golf.py
@@ -1,3 +1,3 @@
 import re
-c=[[int(x)for x in re.split(',|-',l)] for l in open('a').readlines()]
+c=[[int(x)for x in re.split(',|-',l)]for l in open('a').readlines()]
 print(sum((x[0]>=x[2]and x[1]<=x[3])or(x[2]>=x[0]and x[3]<=x[1])for x in c),sum((x[0]<=x[2]<=x[1])or(x[2]<=x[0]<=x[3])for x in c))
