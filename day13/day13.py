5e5388a5de3e161b84e3d92c1e952cab12315db1
Luke Dunekacke <dunekackel@01d-saf-2152i.udev.six3>
2022-12-13 08:14:35 -0700

    cleanup

--- day13/day13.py
+++ day13/day13.py
@@ -65,7 +65,7 @@ def main():
     print(f"Solution 1 for Example is: {ans}")
     ans = solution1("input.txt")
     print(f"Solution 1 for Input is: {ans}")
-# 
+
     ans = solution2("example.txt")
     print(f"Solution 2 for Example is: {ans}")
     ans = solution2("input.txt")
