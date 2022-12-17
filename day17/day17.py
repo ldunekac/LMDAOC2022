286a9590ae8ae029e5a355ad2319831348798f6d
Luke Dunekacke <ldunekacke@gmail.com>
2022-12-16 18:01:31 -0700

    day17 stubs

---
+++ day17/day17.py
@@ -0,0 +1,21 @@
+
+def solution1(file: str) -> int:
+    pass
+
+def solution2(file: str) -> int:
+    pass
+
+def main():
+    ans = solution1("example.txt")
+    print(f"Solution 1 for Example is: {ans}")
+    ans = solution1("input.txt")
+    print(f"Solution 1 for Input is: {ans}")
+
+    ans = solution2("example.txt")
+    print(f"Solution 2 for Example is: {ans}")
+    ans = solution2("input.txt")
+    print(f"Solution 2 for Input is: {ans}")
+
+
+if __name__ == "__main__":
+    main()
