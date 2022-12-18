3aad0553bf55aa029e63461dc6df0e6b6fc1aa04
Luke Dunekacke <ldunekacke@gmail.com>
2022-12-17 23:11:22 -0700

    day 18 part 2

--- day18/day18.py
+++ day18/day18.py
@@ -1,19 +1,58 @@
+adj_indexes = [
+    (1, 0, 0),
+    (-1, 0, 0),
+    (0, 1, 0),
+    (0, -1, 0),
+    (0, 0, 1),
+    (0, 0, -1),
+]
+
+def find_trapped_cubes(cubes):
+    nodes = set(cubes)
+
+    xs = [x for x, _, _ in cubes]
+    minx = min(xs)
+    maxx = max(xs)
+
+    ys = [y for _, y, _ in cubes]
+    miny = min(ys)
+    maxy = max(ys)
+
+    zs = [z for _, _, z in cubes]
+    minz = min(zs)
+    maxz = max(zs)
+
+    possible_inner_cubes = set()
+    outer_cubes = []
+    for i in range(minx, maxx + 1):
+        for j in range(miny, maxy + 1):
+            for k in range(minz, maxz + 1):
+                if not (i, j, k) in nodes:
+                    if i == minx or i == maxx or \
+                        j == miny or j == maxy or \
+                        k == minz or k == maxz:
+                        outer_cubes.append((i,j,k))
+                    else:
+                        possible_inner_cubes.add((i, j, k))
+
+    while len(outer_cubes) > 0:
+        current_node = outer_cubes.pop()
+        (x, y, z) = current_node
+        adj_nodes = [(x + x1, y + y1, z + z1) for x1, y1, z1 in adj_indexes]
+        for adj_node in adj_nodes:
+            if adj_node in possible_inner_cubes:
+                outer_cubes.append(adj_node)
+                possible_inner_cubes.remove(adj_node)
+
+    return list(possible_inner_cubes)
 
 
 def get_total_edges(cubes):
-    outers = [
-        ( 1,  0,  0),
-        (-1,  0,  0),
-        ( 0,  1,  0),
-        ( 0, -1,  0),
-        ( 0,  0,  1),
-        ( 0,  0, -1),
-    ]
     nodes = set(cubes)
 
     total_edges = 0
     for (x, y, z) in nodes:
-        adj_nodes = [(x + x1, y + y1, z + z1) for x1, y1, z1 in outers]
+        adj_nodes = [(x + x1, y + y1, z + z1) for x1, y1, z1 in adj_indexes]
         for adj_node in adj_nodes:
             if adj_node in nodes:
                 total_edges += 1
@@ -30,14 +69,20 @@ def read_input(file):
             cubes.append((int(x), int(y), int(z)))
     return cubes
 
+
 def solution1(file: str) -> int:
     cubes = read_input(file)
     total_edges =  get_total_edges(cubes)
     number_of_outer_faces = (len(cubes) * 6) - total_edges
     return number_of_outer_faces
 
+
 def solution2(file: str) -> int:
-    pass
+    cubes = read_input(file)
+    cubes += find_trapped_cubes(cubes)
+    total_edges = get_total_edges(cubes)
+    number_of_outer_faces = (len(cubes) * 6) - total_edges
+    return number_of_outer_faces
 
 
 def main():
@@ -45,14 +90,11 @@ def main():
     print(f"Solution 1 for Example is: {ans}")
     ans = solution1("input.txt")
     print(f"Solution 1 for Input is: {ans}")
-    #
-    # ans = solution2("example.txt")
-    # print(f"Solution 2 for Example is: {ans}")
-    # start = time.perf_counter()
-    # ans = solution2("input.txt")
-    # end = time.perf_counter()
-    # print(f"Solution 2 for Input is: {ans}")
-    # print(f"Elapsed Time {end - start}")
+
+    ans = solution2("example.txt")
+    print(f"Solution 2 for Example is: {ans}")
+    ans = solution2("input.txt")
+    print(f"Solution 2 for Example is: {ans}")
 
 
 if __name__ == "__main__":
