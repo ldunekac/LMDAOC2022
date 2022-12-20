896c402e2108a2ed81485f55887b4edbc0cc120a
Luke Dunekacke <ldunekacke@gmail.com>
2022-12-20 09:28:21 -0700

    parly done

--- day20/day20.py
+++ day20/day20.py
@@ -2,6 +2,7 @@ from copy import deepcopy
 
 
 def unencrypt_file(encrypted_list):
+    list_len = len(encrypted_list)
     unencrypted_list = deepcopy(encrypted_list)
     # The index of mapping aligns with the index of encrypted_list
     # The value of mapping, is the index in the unencrypted list 
@@ -10,6 +11,38 @@ def unencrypt_file(encrypted_list):
     # Then the mapping[4] = 7. 
     # We can say that encrypted_list[4] = unencrypeted_list[mapping[4]]
     mapping = [x for x in range(len(unencrypted_list))]
+
+    for ind, num in enumerate(encrypted_list):
+        print("Current array status")
+        print(encrypted_list)
+        print(mapping)
+        print(unencrypted_list)
+        
+        num_idx_in_unencrypted_list = mapping[ind]
+        assert num == unencrypted_list[num_idx_in_unencrypted_list]
+
+        new_index = (num_idx_in_unencrypted_list + num) % list_len
+        
+        if new_index > num_idx_in_unencrypted_list:
+            # update the unencrypted map
+            for i in range(num_idx_in_unencrypted_list, new_index):
+                print(i)
+                unencrypted_list[i] = unencrypted_list[i + 1]
+            unencrypted_list[new_index] = num
+            # move the number in the unencryped list to the right
+            # all values in between get shifted one index down ( -1)
+            for i in range(list_len):
+                if num_idx_in_unencrypted_list <= mapping[i] <= new_index:
+                    mapping[i] -= 1
+            
+            mapping[ind] = new_index 
+            for idx2, num2 in enumerate(encrypted_list):
+                assert num2 == unencrypted_list[mapping[idx2]]
+        else:
+            print("not yet")
+            raise
+        
+
 
 def read_input(file):
     with open(file, "r") as f:
